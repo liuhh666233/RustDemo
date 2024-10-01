03cd1f2decd1aab05155cd5a54a8b4d78e6aeebe
lxb <liuxiaobo666233@gmail.com>
2024-10-01 15:10:40 +0800

    Initializing repo.

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "A devShell example";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+      in {
+        devShells.default = with pkgs;
+          mkShell {
+            buildInputs =
+              [ openssl pkg-config  fd rust-bin.beta.latest.default ];
+
+            # Setting up the environment variables you need during
+            # development.
+            shellHook = let 
+            icon = "f121";
+            name = "RustDemo";
+            in ''
+              export PS1="$(echo -e '\u${icon}') {\[$(tput sgr0)\]\[\033[38;5;228m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]} (${name}) \\$ \[$(tput sgr0)\]"
+            '';
+          };
+      });
+}
